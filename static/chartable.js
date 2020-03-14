81dd258becc11432562bcc869e5b660c68f764e9
ephinea4haven <ephinea4haven@outlook.com>
2020-03-14 14:56:03 +0800

    add 200 stat

--- static/chartable.js
+++ static/chartable.js
@@ -4,31 +4,40 @@ $(function () {
 
     var t = new CharData();
 
-    var humar = t.humar.lv;
 
-    for (lv in humar) {
+    for(clazz in t.clazz){
 
-        /*
-        <tr>
-        <th scope="row">1</th>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-    </tr>
-         */
-        let stat = humar[lv];
-        var row = "" +
-            "<tr>" +
-            "<td>" + lv + "</td>" +
-            "<td>" + stat[0] + "</td>" +
-            "<td>" + stat[1] + "</td>" +
-            "<td>" + stat[2] + "</td>" +
-            "<td>" + stat[3] + "</td>" +
-            "<td>" + stat[4] + "</td>" +
-            "<td>" + stat[5] + "</td>" +
-            "<td>" + stat[6] + "</td>" +
-            "</tr>";
-        $('#humar').append(row)
+        var role = t[clazz].lv;
+        for (lv in role) {
+
+            /*
+            <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+        </tr>
+             */
+            let stat = role[lv];
+            var row = "" +
+                "<tr>" +
+                "<td>" + lv + "</td>" +
+                "<td>" + stat[0] + "</td>" +
+                "<td>" + stat[1] + "</td>" +
+                "<td>" + stat[2] + "</td>" +
+                "<td>" + stat[3] + "</td>" +
+                "<td>" + stat[4] + "</td>" +
+                "<td>" + stat[5] + "</td>" +
+                "<td>" + stat[6] + "</td>" +
+                "</tr>";
+
+            var id = "#"+clazz;
+            $(id).append(row)
+        }
     }
+
+    $("#topArrow").click(function(){
+        $(document).scrollTop(0);
+    })
 
 });
