db5c9f11f8c359be2b5e05998039f6083a1dbc06
wangzhen <wangzhen@kuihai.net>
2025-10-22 08:58:26 +0800

    chord: remove chardata viewer

--- chardata/README.md
+++
@@ -1,4 +0,0 @@
-# PSOBBCharacterItemViewer
-
-Forum
-https://www.pioneer2.net/community/threads/character-item-viewer-on-the-web.22882/
--- chardata/js/view/event/changetheme.js
+++
@@ -1,7 +0,0 @@
-function clickChangeTheme(value) {
-    document.getElementById("stylesheet").href = `./css/${value}.css`;
-    console.log("change to:" + value);
-    localStorage.setItem("theme", value);
-    refreshVolume();
-    pushedPageColoer(`page${this.currentData["searching"][0]}${this.currentData["searching"][1]}`);
-}
